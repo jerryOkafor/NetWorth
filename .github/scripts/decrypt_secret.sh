fe09bf17b7c3d1a7f205afe9a2305a21bfb836ca
Jerry Okafor <jerryhanksokafor@gmail.com>
2023-10-08 21:48:18 -0600

    feat : add fastlane

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,7 +5,7 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 
-cho $PWD
+echo $PWD
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output ../iosApp/Configuration/Config.xcconfig ../iosApp/Configuration/Config.xcconfig.gpg
