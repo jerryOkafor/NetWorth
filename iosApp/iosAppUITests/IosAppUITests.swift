38e37d9fe485103e26cb5c4c6feb50b825532d6b
Jerry Okafor <jerryhanksokafor@gmail.com>
2023-10-13 10:56:48 -0600

    chore : benchmark .konan build cache options.

--- iosApp/iosAppUITests/IosAppUITests.swift
+++ iosApp/iosAppUITests/IosAppUITests.swift
@@ -34,7 +34,7 @@ final class IosAppUITests: XCTestCase {
   }
 
   func testLaunchPerformance() throws {
-    if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+    if #available(iOS 14.0, *) {
       // This measures how long it takes to launch your application.
       measure(metrics: [XCTApplicationLaunchMetric()]) {
         XCUIApplication().launch()
